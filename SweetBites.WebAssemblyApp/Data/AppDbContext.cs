162dba37ae6dc46dd698d23f9a9656747bc96a95
Ervis Trupja <ervistrupja@outlook.com>
2023-06-03 22:29:41 +0200

    Revert "Removed redundant files"
    This reverts commit d4e9b4a43ddbf24a87b1cfa54a16b3e865ce2a67.

---
+++ SweetBites.WebAssemblyApp/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SweetBites.ServerApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Sweet> Sweets { get; set; }
+    }
+}
